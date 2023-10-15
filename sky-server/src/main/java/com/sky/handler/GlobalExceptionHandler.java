c8a3477eba1b3e91f35a57408185a9222c82f31b
yibai100 <1250349299@qq.com>
2023-10-15 23:02:47 +0800

    同名异常处理

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +26,29 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
+
+
+    /**
+     * 处理用户名重复异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //Duplicate entry 'abc' for key 'employee.idx_username'
+        String msg=ex.getMessage();
+        if(msg.contains("Duplicate entry ")){
+            String[] split=msg.split(" ");
+            String userName=split[2];
+            String showMsg="用户名"+userName+"已存在";
+            return Result.error(showMsg);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+
+
+    }
+
 
 }
