a9d1f110a7b178c1d2eae6df6d9498be857a2d8b
yibai100 <1250349299@qq.com>
2023-11-06 23:41:56 +0800

    实现微信登录功能（未测试）

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据openid查询用户
+     * @param openid
+     * @return
+     */
+    @Select("SELECT * from user where openid=#{openid}")
+    User getUserById(String openid);
+
+    void insert(User user);
+}
